b098adabedce06199b05f60e80c32c0769b3ce46
Maximiliano <maxinunez2455@gmail.com>
2025-08-20 14:51:17 -0300

    Avance con ejercicios de POO

---
+++ conceptos-kotlin/app/src/main/java/com/MaxiNunez24/curso_android/POO/ejerciciosPOO/CuentaBancaria.kt
@@ -0,0 +1,25 @@
+package com.MaxiNunez24.curso_android.POO.ejerciciosPOO
+
+class CuentaBancaria(val titular: String, var saldo: Double) {
+
+    fun depositar(cantidad: Double) {
+        saldo = saldo + cantidad
+    }
+
+    fun extraer(cantidad: Double): Double {
+        return if(saldo >= cantidad) {
+            saldo = saldo - cantidad
+            cantidad
+        }
+        else {
+            println("No tiene suficiente saldo para extraer $$cantidad")
+            mostrarSaldo()
+            0.0
+        }
+    }
+
+    fun mostrarSaldo() {
+        println("Saldo actual de $titular: $$saldo")
+    }
+
+}
